928c98dd9722e53efce087c10fe45dd2680a26d1
Paul <pauldevaublanc@gmail.com>
2019-09-18 09:11:34 +0200

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Test integration Fidesio
+# Test integration Havas
 
 ### Installation
 
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,47 +1,4 @@
-import '../styles/index.scss';
 
-import $ from 'jquery';
-
-$(document).ready(function () {
-    $('#js-header-navbar ul li').click(function() {
-        $('#js-header-navbar').find('li.active-link').removeClass('active-link');
-        $(this).addClass('active-link');
-
-        let index = $(this).index()+1;
-        $('#js-header-wrapper').find('.active-article').removeClass('active-article');
-        $(`#js-article${index}`).addClass('active-article');
-
-    });
-
-
-    var slideIndex = 1;
-    showSlides(slideIndex);
-
-    // Next/previous controls
-
-    $(".next").click(function(){
-        showSlides(slideIndex += -1);
-    });
-
-    $(".prev").click(function(){
-        showSlides(slideIndex += 1);
-    });
-
-
-    function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("slide");
-
-    if (n > slides.length) {slideIndex = 1;} 
-    if (n < 1) {slideIndex = slides.length;}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; 
-    }
-
-    slides[slideIndex-1].style.display = "flex"; 
-
-    }
-});
 
 
 
