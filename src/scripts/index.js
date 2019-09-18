e67edb0754b1c186ce29e8310d05cf0ef66c5de4
Paul <pauldevaublanc@gmail.com>
2019-09-18 23:43:22 +0200

    create video header part

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,18 @@
+window.addEventListener("scroll", ()=> {
+    const navbar = document.getElementById("navbar");
+    if (window.scrollY > 50) {
+        navbar.style.top = 0;
+    } else {
+        navbar.style.top = "calc(100vh - 50px)";
+    }
+});
 
 
+const video = document.getElementById("js-action-video");
 
-
+video.addEventListener("click", ()=> {
+    document.getElementById("js-header-visuel").classList.add("slide-out");
+});
 
 
 
